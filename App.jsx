a3cbe14fd8d849763aaacc4aff678c082c5e737f
JainAyush911 <122337690+JainAyush911@users.noreply.github.com>
2025-01-01 16:45:14 +0530

    Created Home Screen with Navigation

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import HomeScreen from "./src/screen/HomeScreen";
-import LoginScreen from "./src/screen/LoginScreen";
-import SignupScreen from "./src/screen/SignupScreen";
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from './src/screen/HomeScreen';
+import LoginScreen from './src/screen/LoginScreen';
+// import SignupScreen from './src/screen/SignupScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,11 +14,10 @@ const App = () => {
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
-        }}
-      >
-        <Stack.Screen name={"HOME"} component={HomeScreen} />
-        <Stack.Screen name={"LOGIN"} component={LoginScreen} />
-        <Stack.Screen name="SIGNUP" component={SignupScreen} />
+        }}>
+        <Stack.Screen name={'HOME'} component={HomeScreen} />
+        <Stack.Screen name={'LOGIN'} component={LoginScreen} />
+        {/* <Stack.Screen name="SIGNUP" component={SignupScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
